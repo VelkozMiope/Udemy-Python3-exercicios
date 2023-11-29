3c7c289c294dabe013690ceb91ce3905906cdb53
Márcio Paulin <paulin.marcio@outlook.com>
2023-11-29 02:22:47 -0300

    Exercício comparação de valores

---
+++ Exercício_valores.py
@@ -0,0 +1,11 @@
+# Exercício de comparação de valores
+
+primeiro_valor = input('Digite o primeiro valor: ')
+segundo_valor = input('Digite o segundo valor: ')
+
+if primeiro_valor > segundo_valor:
+    print(f'{primeiro_valor} é maior que {segundo_valor}')
+elif primeiro_valor < segundo_valor:
+    print(f'{segundo_valor} é maior que {primeiro_valor}')
+else:
+    print(f'{primeiro_valor} e {segundo_valor} são iguais!')
